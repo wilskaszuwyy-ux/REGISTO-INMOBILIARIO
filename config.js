6a3e91aac3f7a9034f5ed20d7930a456b64444a0
wilskaszuwyy-ux <wilskaszuwyy@gmail.com>
2026-07-15 12:27:01 -0500

    Add files via upload

--- config.js
+++ config.js
@@ -4,5 +4,5 @@
  * privado de Google Sheets.
  */
 window.INMOCONECTA_CONFIG = {
-  appsScriptUrl: "https://script.google.com/macros/s/AKfycbyKXx7SoTIcYbDszKQs-3kHQ8mOUC5c_saF4z2v_aQfOFUC1uhF5kXc3xaVzx834hC6-Q/exec"
+  appsScriptUrl: "https://script.google.com/macros/s/AKfycbxceYSxQilERDQZ-sIW5I4dvvJ7govQf7Rm1i7WWbhxdcmZRAbHxr_REe87JievO6eSSw/exec"
 };
